0485041a7338ec2a42cc9c4e1dfcf6c4159cd4c5
Nikolay Nemshilov <nemshilov@gmail.com>
2013-01-26 12:56:25 +1100

    Moving the Matcher instantiation behind Matcher factory

--- lib/minitest/mustwonted/matcher.rb
+++ lib/minitest/mustwonted/matcher.rb
@@ -4,6 +4,14 @@
 class MiniTest::MustWonted::Matcher
   attr_accessor :subject, :type
 
+  def self.must(subject, *args)
+    new(subject, :must)
+  end
+
+  def self.wont(subject, *args)
+    new(subject, :wont)
+  end
+
   def initialize(subject, type)
     @subject = subject
     @type    = type
--- lib/minitest/mustwonted/object.rb
+++ lib/minitest/mustwonted/object.rb
@@ -10,7 +10,7 @@ class Object
   #     ....
   #
   def must(*args)
-    MiniTest::MustWonted::Matcher.new(self, :must)
+    MiniTest::MustWonted::Matcher.must(self, *args)
   end
 
   #
@@ -21,6 +21,6 @@ class Object
   #     ....
   #
   def wont(*args)
-    MiniTest::MustWonted::Matcher.new(self, :wont)
+    MiniTest::MustWonted::Matcher.wont(self, *args)
   end
 end
